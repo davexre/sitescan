f89dd09562c1abd657435582e6b310f8cbd32b4f
davexre <63078133+davexre@users.noreply.github.com>
2021-01-25 14:38:24 -0600

    Update webhandler.go

--- webhandler/webhandler.go
+++ webhandler/webhandler.go
@@ -11,27 +11,11 @@ type HTTPClient interface {
 	Do(req *http.Request) (*http.Response, error)
 }
 
-// MockClient can be used for mocking an HTTPClient response
-type MockClient struct {
-	DoFunc func(req *http.Request) (*http.Response, error)
-}
-
-// Do satisfies the interface's requriement for a Do function, and returns the results
-// of the function pointed to by GetDoFunc below.
-func (m *MockClient) Do(req *http.Request) (*http.Response, error) {
-	return GetDoFunc(req)
-}
-
 var (
 	// Client defines which HTTP interface will be used by HTTPHandler. By default, this is
 	// set to http.Client{} as part of the init function, but it can be changed to provide
 	// a mock HTTP response for testing purposes
 	Client HTTPClient
-
-	// GetDoFunc fetches the mock client's `Do` func - essentially, you can set this
-	// variable to an anonymous function within the test that receives an http.Request
-	// pointer, and returns an http.Response pointer and/or an error
-	GetDoFunc func(req *http.Request) (*http.Response, error)
 )
 
 func init() {
