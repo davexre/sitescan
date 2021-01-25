b8e9d72f5529768e390df76f691b694fd2432014
davexre <63078133+davexre@users.noreply.github.com>
2021-01-25 16:38:25 -0600

    Implement TestWalkLink

--- sitescan_test.go
+++ sitescan_test.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/davexre/sitescan/mocks"
+	"github.com/davexre/sitescan/webhandler"
+	"github.com/davexre/syncedData"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"testing"
 )
@@ -48,4 +54,51 @@ func TestCompareMaps(t *testing.T) {
 	}
 
 	assert.Equal(t, output[:], expectedOutput[:])
+}
+
+// Test site structure
+// someurl.com/
+//             "Name"
+//             dir1/
+//             dir1/file11
+//             dir2/
+//             dir2/file21
+//             file3
+func TestWalkLink(t *testing.T) {
+
+	response := ""
+	url := "http://someurl.com/"
+	var testmap = make(map[string]string)
+	var counter syncedData.Counter
+
+	webhandler.Client = &mocks.MockClient{}
+	mocks.GetDoFunc = func(req *http.Request) (*http.Response, error) {
+		urlReq := req.URL.String()
+		switch {
+		case urlReq == url:
+			response = `<a href="name">Name</a><a href="dir1/">dir1</a><a href="dir2/">dir2/</a><a href="file3.mp4">file3.mp4</a>`
+		case urlReq == url+"dir1/":
+			response = `<a href="file11.mp3">file11.mp3</a>`
+		case urlReq == url+"dir2/":
+			response = `<a href="file21.jpg">file21.jpg</a>`
+		default:
+			fmt.Printf("TestWalkLink - Invalid test URL - exiting\n")
+			os.Exit(1)
+		}
+		r := ioutil.NopCloser(bytes.NewReader([]byte(response)))
+		return &http.Response{
+			StatusCode: 200,
+			Body:       r,
+		}, nil
+	}
+
+	walkLink(url, "", "", &testmap, "", "", &counter)
+
+	/// now, check our map!
+	assert.Equal(t, testmap["dir1/"], "dir1/", "map entry incorrect")
+	assert.Equal(t, testmap["dir1/file11.mp3"], "dir1/file11.mp3", "map entry incorrect")
+	assert.Equal(t, testmap["dir2/"], "dir2/", "map entry incorrect")
+	assert.Equal(t, testmap["dir2/file21.jpg"], "dir2/file21.jpg", "map entry incorrect")
+	assert.Equal(t, testmap["file3.mp4"], "file3.mp4", "map entry incorrect")
+
 }
