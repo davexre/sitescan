3fd219e876139488a7a3581e02e4dfb7a0b4a698
davexre <63078133+davexre@users.noreply.github.com>
2021-01-25 14:23:49 -0600

    Add test for HTTPHandler
    Also, prototype testing for web tests in sitescan.go by first testing in TestHTTPHandler

--- webhandler/webhandler_test.go
+++ webhandler/webhandler_test.go
@@ -1,10 +1,17 @@
 package webhandler
 
 import (
+	"bytes"
 	"github.com/davexre/sitescan/mocks"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
 	"testing"
 )
+
+func init() {
+	Client = &mocks.MockClient{}
+}
 
 func TestValidateURL(t *testing.T) {
 	assert := assert.New(t)
@@ -29,4 +36,36 @@ func TestValidateURL(t *testing.T) {
 		}
 	}
 
+}
+
+func TestHTTPHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	var tests = []struct {
+		url         string
+		expectError bool
+		response    string
+	}{
+		{"http://testurl.com", false, `<a href="dir1">dir1</a><a href=dir2>dir2</a>`},
+		{"\"http://bogus.com\"", true, `<a href="dir1">dir1</a><a href=dir2>dir2</a>`},
+	}
+	for _, test := range tests {
+
+		r := ioutil.NopCloser(bytes.NewReader([]byte(test.response)))
+		mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
+			return &http.Response{
+				StatusCode: 200,
+				Body:       r,
+			}, nil
+		}
+
+		res, err := HTTPHandler(test.url, "", "")
+		if test.expectError {
+			assert.NotNil(err)
+			assert.Nil(res)
+		} else {
+			assert.NotNil(res)
+			assert.Nil(err)
+		}
+	}
 }
